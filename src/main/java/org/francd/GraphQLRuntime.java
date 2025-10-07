673bf9dcc12943c8c3628b3bca4a3cfb598a8693
francd <fcamadi@gmail.com>
2025-10-07 21:38:47 +0200

    initial commit

---
+++ src/main/java/org/francd/GraphQLRuntime.java
@@ -0,0 +1,13 @@
+package org.francd;
+
+ import graphql.GraphQL;
+
+ public class GraphQLRuntime {
+
+     private final GraphQL graphql;
+
+     public GraphQLRuntime() {
+         graphql = null;
+     }
+
+}
