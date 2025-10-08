b2b9aff2800cc50ee62d38766e67e5eb73bd7703
francd <fcamadi@gmail.com>
2025-10-08 18:54:28 +0200

    add tests for GraphQLRuntime

---
+++ src/main/test/java/org/francd/GraphQLRuntimeTest.java
@@ -0,0 +1,68 @@
+package org.francd;
+
+import graphql.ExecutionResult;
+import graphql.validation.ValidationError;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class GraphQLRuntimeTest {
+
+    private final GraphQLRuntime runtime;
+
+    GraphQLRuntimeTest() throws IOException {
+        runtime = new GraphQLRuntime();
+    }
+
+    @Test
+    public void validQuery() {
+        // Given
+        var query = """
+            {
+                countries {
+                    name
+                    population
+                    capital {
+                        name
+                        population
+                    }
+                }
+            }
+        """;
+
+        // When
+        ExecutionResult result = runtime.execute(query);
+
+        // Then
+        assertThat(result).isNotNull();
+        assertThat(result.getErrors()).isEmpty();
+    }
+
+    @Test
+    void invalidQuery() {
+        // Given
+        var query = """
+            {
+                countries {
+                    latitude  # does not exist in schema
+                    name
+                    population
+                    capital {
+                        name
+                        population
+                    }
+                }
+            }
+        """;
+
+        // When
+        ExecutionResult result = runtime.execute(query);
+
+        // Then
+        assertThat(result).isNotNull();
+        assertThat(result.getErrors()).isNotEmpty();
+        assertThat(result.getErrors().getFirst()).isInstanceOf(ValidationError.class);
+    }
+}
