66e4b121d735825eb5c8ac560f34373e80d63bba
francd <fcamadi@gmail.com>
2025-10-08 19:05:36 +0200

    add test to include new field Province

--- src/main/test/java/org/francd/GraphQLRuntimeTest.java
+++ src/main/test/java/org/francd/GraphQLRuntimeTest.java
@@ -41,6 +41,32 @@ class GraphQLRuntimeTest {
     }
 
     @Test
+    public void validQueryProvince() {
+        // Given
+        var query = """
+            {
+                countries {
+                    name
+                    population
+                    capital {
+                        name
+                    }
+                    provinces {
+                        name
+                    }
+                }
+            }
+        """;
+
+        // When
+        ExecutionResult result = runtime.execute(query);
+
+        // Then
+        assertThat(result).isNotNull();
+        assertThat(result.getErrors()).isEmpty();
+    }
+
+    @Test
     void invalidQuery() {
         // Given
         var query = """
