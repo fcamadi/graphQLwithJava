4d3dc8d9100e2393dba5a2d6a7fea8e1de4a7310
francd <fcamadi@gmail.com>
2025-10-13 14:53:12 +0200

    small change in data fetcher for province of a city

--- src/main/java/org/francd/fetchers/StaticProvinceDataFetcher.java
+++ src/main/java/org/francd/fetchers/StaticProvinceDataFetcher.java
@@ -10,17 +10,17 @@ import java.util.Map;
 public class StaticProvinceDataFetcher implements DataFetcher<Province> {
 
     private final static Map<String, Province> PROVINCES = Map.of(
-            "Paris", new Province("Île-de-France", 12082144, "Paris", 12345),
+            "Île-de-France", new Province("Île-de-France", 12082144, "Paris", 12345),
             "Madrid", new Province("Madrid", 6726640,  "Madrid", 12345),
             "Bern", new Province("Bern", 1043132,  "Bern", 2345),
-            "Lisbon", new Province("Prov. Lisbon", 189545, "Lisbon", 2345)
+            "Prov. Lisbon", new Province("Prov. Lisbon", 189545, "Lisbon", 2345)
     );
 
     @Override
     public Province get(DataFetchingEnvironment environment) {
         City sourceCity = environment.getSource();
-        var capitalName = sourceCity.name();
-        return PROVINCES.get(capitalName);
+        var provinceName = sourceCity.province();
+        return PROVINCES.get(provinceName);
     }
 }
 
