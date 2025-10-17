71ee56936305bb1efc0857106ef8aab16790599f
francd <fcamadi@gmail.com>
2025-10-17 20:20:47 +0200

    add enum for Continent

---
+++ src/main/java/org/francd/model/Continent.java
@@ -0,0 +1,24 @@
+package org.francd.model;
+
+public enum Continent {
+    Europe,
+    Asia,
+    Africa,
+    SouthAmerica ("South America"),
+    NorthAmerica ("North America"),
+    Oceania("Australia/Oceania");
+
+    private final String dbName;
+
+    Continent(String dbName) {
+        this.dbName = dbName;
+    }
+
+    Continent() {
+        this.dbName = this.dbName();
+    }
+
+    public String dbName() {
+        return this.dbName;
+    }
+}
